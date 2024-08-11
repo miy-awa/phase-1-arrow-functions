8b641282e4b5cb0539f6c20615c7f100afefc94e
miyawa <raymiyawa6@gmail.com>
2024-08-11 10:19:52 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,13 @@
+/*function foo() {
+    return 'bar';
+}*/
+
+/*const add = (parameter1, parameter2) => parameter1 + parameter2;
+add(2,3);*/
+
+const sum = (parameter1, parameter2) => {
+    console.log(`Adding ${parameter1}`);
+    console.log(`Adding ${parameter2}`);
+    return parameter1 + parameter2;
+}
+sum(1,2);
